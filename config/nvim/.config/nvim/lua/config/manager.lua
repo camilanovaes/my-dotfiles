fac3a78439b3bcba2adcc7c763bc15dd9479332d
Camila Novaes <novaes.csw@gmail.com>
2025-10-26 12:44:58 -0300

    nvim: add vim-slime for repl interaction

--- config/nvim/.config/nvim/lua/config/manager.lua
+++ config/nvim/.config/nvim/lua/config/manager.lua
@@ -147,6 +147,20 @@ require('lazy').setup({
   -- Copilot (testing)
   'github/copilot.vim',
 
+  -- REPL
+  {
+    'jpalardy/vim-slime',
+    config = function()
+      vim.g.slime_target = "tmux"
+      vim.g.slime_default_config = {
+        socket_name = "default",
+        target_pane = 2
+      }
+      vim.g.slime_dont_ask_default = 2
+      vim.g.slime_python_ipython = 1
+    end
+  },
+
   -- Remote development
   {
     "amitds1997/remote-nvim.nvim",
